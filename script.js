c1e9167481d7311c59cceafaa90e2940552c2f88
Jerrylejer <81814979+Jerrylejer@users.noreply.github.com>
2022-11-27 11:17:41 +0100

    MISE EN PLACE DU SLIDER EN PAGE ACCUEIL + RESPONSIVE OK MISE EN PLACE DES LIENS ACCUEIL => CATEGORIES ET CATEGORIE FEMME POUR DEMONSTRATION

--- script.js
+++ script.js
@@ -19,4 +19,57 @@ function navigationHide() {
     burgerBarre1.classList.toggle('rotate1');
     burgerBarre2.classList.toggle('opacity');
     burgerBarre3.classList.toggle('rotate2');
+};
+
+//? Animation des slider accueil et catégories
+// Je stocke mes éléments 1er carrousel, second carrousel, troisième carrousel
+const carrousel1 = document.getElementById('carrousel1');
+const carrousel2 = document.getElementById('carrousel2');
+const carrousel3 = document.getElementById('carrousel3');
+const precedent = document.getElementById('chevronG');
+const suivant = document.getElementById('chevronD');
+// console.log(carrousel1);
+// console.log(carrousel2);
+// console.log(carrousel3);
+
+// Les actions au clics => addEvntListener sur les icônes prec & suite
+precedent.addEventListener('click', slideGauche);
+suivant.addEventListener('click', slideDroite);
+
+// Au click, je veux que mon carrousel2 "on" se mette "off", carrousel1 se mette "on"
+function slideGauche() {
+    // Premier cas de figure, je suis sur le carrousel2
+    if(carrousel2.classList.contains('on')) {
+        // Disparition carrousel 2
+        carrousel2.classList.toggle('on');
+        carrousel2.classList.toggle('off');
+        // Apparition carrousel 1
+        carrousel1.classList.toggle('off');
+        carrousel1.classList.toggle('on');
+    } else if (carrousel3.classList.contains('on')) {
+        // Disparition carrousel 3
+        carrousel3.classList.toggle('on');
+        carrousel3.classList.toggle('off');
+        // Apparition carrousel 2
+        carrousel2.classList.toggle('off');
+        carrousel2.classList.toggle('on');
+    }
+}
+function slideDroite() {
+    // Premier cas de figure, je suis sur le carrousel2
+    if(carrousel2.classList.contains('on')) {
+        // Disparition carrousel 2
+        carrousel2.classList.toggle('on');
+        carrousel2.classList.toggle('off');
+        // Apparition carrousel 3
+        carrousel3.classList.toggle('off');
+        carrousel3.classList.toggle('on');
+    } else if (carrousel1.classList.contains('on')) {
+        // Disparition carrousel 3
+        carrousel1.classList.toggle('on');
+        carrousel1.classList.toggle('off');
+        // Apparition carrousel 2
+        carrousel2.classList.toggle('off');
+        carrousel2.classList.toggle('on');
+    } 
 }
