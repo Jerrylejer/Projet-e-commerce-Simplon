8e550a88c36894df5ba98162a72495de583acd1f
Jerrylejer <81814979+Jerrylejer@users.noreply.github.com>
2022-11-30 22:12:01 +0100

    MISE EN PLACE DU CARROUSEL RESPONVIVE IMAGE PAR IMAGE

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ function navigationHide() {
     burgerBarre3.classList.toggle('rotate2');
 };
 
+// CARROUSEL PAR LOT DE 4
 //? Animation des slider accueil et catégories
 // Je stocke mes éléments 1er carrousel, second carrousel, troisième carrousel
 const carrousel1 = document.getElementById('carrousel1');
@@ -55,6 +56,7 @@ function slideGauche() {
         carrousel2.classList.toggle('on');
     }
 }
+
 function slideDroite() {
     // Premier cas de figure, je suis sur le carrousel2
     if(carrousel2.classList.contains('on')) {
@@ -72,4 +74,146 @@ function slideDroite() {
         carrousel2.classList.toggle('off');
         carrousel2.classList.toggle('on');
     } 
+}
+
+// CARROUSEL RESPONSIVE 
+//? Animation des slider accueil et catégories
+// Je stocke mes éléments du carrousel
+const prec = document.getElementById('chevronGauche');
+const suiv = document.getElementById('chevronDroit');
+const image1 = document.getElementById('image1');
+const image2 = document.getElementById('image2');
+const image3 = document.getElementById('image3');
+const image4 = document.getElementById('image4');
+const image5 = document.getElementById('image5');
+const image6 = document.getElementById('image6');
+const image7 = document.getElementById('image7');
+const image8 = document.getElementById('image8');
+const image9 = document.getElementById('image9');
+const image10 = document.getElementById('image10');
+const image11 = document.getElementById('image11');
+const image12 = document.getElementById('image12');
+
+// Les actions au clics => addEvntListener sur les icônes prec & suite
+prec.addEventListener('click', slideG);
+suiv.addEventListener('click', slideD);
+
+// Au click, je veux que mon carrousel2 "on" se mette "off", carrousel1 se mette "on"
+function slideG() {
+    // Premier cas de figure, je suis sur le carrousel2
+    if(image12.classList.contains('on')) {
+        image12.classList.toggle('on');
+        image12.classList.toggle('off');
+        image11.classList.toggle('off');
+        image11.classList.toggle('on');
+    } else if (image11.classList.contains('on')) {
+        image11.classList.toggle('on');
+        image11.classList.toggle('off');
+        image10.classList.toggle('off');
+        image10.classList.toggle('on');
+    } else if (image10.classList.contains('on')) {
+        image10.classList.toggle('on');
+        image10.classList.toggle('off');
+        image9.classList.toggle('off');
+        image9.classList.toggle('on');
+    } else if (image9.classList.contains('on')) {
+        image9.classList.toggle('on');
+        image9.classList.toggle('off');
+        image8.classList.toggle('off');
+        image8.classList.toggle('on');
+    } else if (image8.classList.contains('on')) {
+        image8.classList.toggle('on');
+        image8.classList.toggle('off');
+        image7.classList.toggle('off');
+        image7.classList.toggle('on');
+    } else if (image7.classList.contains('on')) {
+        image7.classList.toggle('on');
+        image7.classList.toggle('off');
+        image6.classList.toggle('off');
+        image6.classList.toggle('on');
+    } else if (image6.classList.contains('on')) {
+        image6.classList.toggle('on');
+        image6.classList.toggle('off');
+        image5.classList.toggle('off');
+        image5.classList.toggle('on');
+    } else if (image5.classList.contains('on')) {
+        image5.classList.toggle('on');
+        image5.classList.toggle('off');
+        image4.classList.toggle('off');
+        image4.classList.toggle('on');
+    } else if (image4.classList.contains('on')) {
+        image4.classList.toggle('on');
+        image4.classList.toggle('off');
+        image3.classList.toggle('off');
+        image3.classList.toggle('on');
+    } else if (image3.classList.contains('on')) {
+        image3.classList.toggle('on');
+        image3.classList.toggle('off');
+        image2.classList.toggle('off');
+        image2.classList.toggle('on');
+    } else if (image2.classList.contains('on')) {
+        image2.classList.toggle('on');
+        image2.classList.toggle('off');
+        image1.classList.toggle('off');
+        image1.classList.toggle('on');
+    }
+}
+
+function slideD() {
+    if(image1.classList.contains('on')) {
+        image1.classList.toggle('on');
+        image1.classList.toggle('off');
+        image2.classList.toggle('off');
+        image2.classList.toggle('on');
+    } else if (image2.classList.contains('on')) {
+        image2.classList.toggle('on');
+        image2.classList.toggle('off');
+        image3.classList.toggle('off');
+        image3.classList.toggle('on');
+    } else if (image3.classList.contains('on')) {
+        image3.classList.toggle('on');
+        image3.classList.toggle('off');
+        image4.classList.toggle('off');
+        image4.classList.toggle('on');
+    } else if (image4.classList.contains('on')) {
+        image4.classList.toggle('on');
+        image4.classList.toggle('off');
+        image5.classList.toggle('off');
+        image5.classList.toggle('on');
+    } else if (image5.classList.contains('on')) {
+        image5.classList.toggle('on');
+        image5.classList.toggle('off');
+        image6.classList.toggle('off');
+        image6.classList.toggle('on');
+    } else if (image6.classList.contains('on')) {
+        image6.classList.toggle('on');
+        image6.classList.toggle('off');
+        image7.classList.toggle('off');
+        image7.classList.toggle('on');
+    } else if (image7.classList.contains('on')) {
+        image7.classList.toggle('on');
+        image7.classList.toggle('off');
+        image8.classList.toggle('off');
+        image8.classList.toggle('on');
+    } else if (image8.classList.contains('on')) {
+        image8.classList.toggle('on');
+        image8.classList.toggle('off');
+        image9.classList.toggle('off');
+        image9.classList.toggle('on');
+    } else if (image9.classList.contains('on')) {
+        image9.classList.toggle('on');
+        image9.classList.toggle('off');
+        image10.classList.toggle('off');
+        image10.classList.toggle('on');
+    } else if (image10.classList.contains('on')) {
+        image10.classList.toggle('on');
+        image10.classList.toggle('off');
+        image11.classList.toggle('off');
+        image11.classList.toggle('on');
+    } else if (image11.classList.contains('on')) {
+        image11.classList.toggle('on');
+        image11.classList.toggle('off');
+        image12.classList.toggle('off');
+        image12.classList.toggle('on');
+    }
 }
